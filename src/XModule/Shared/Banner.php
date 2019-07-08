447dc2925c18a4e5b2e43327d6b7529251f58ff6
Wose <Jacksonteather@hotmail.com>
2019-07-08 12:47:30 -0400

    ⚡️ Complete banner element for metadata

--- src/XModule/Shared/Banner.php
+++ src/XModule/Shared/Banner.php
@@ -3,6 +3,7 @@
 namespace XModule\Shared;
 
 use \XModule\Constants\BannerType;
+use \XModule\Traits\WithLink;
 
 const DEFAULT_BANNER_OPTIONS = [
   'message' => null,
@@ -12,14 +13,43 @@ const DEFAULT_BANNER_OPTIONS = [
 
 class Banner
 {
+  use WithLink;
 
   public function __construct(array $options = DEFAULT_BANNER_OPTIONS)
   {
+    self::initLink($options);
+
+    if (isset($options['message'])) {
+      $this->setMessage($options['message']);
+    }
+    if (isset($options['type'])) {
+      $this->setType($options['type']);
+    }
+  }
+
+  public function setMessage(string $message)
+  {
+    $this->message = $message;
+  }
+
+  public function setType(string $type)
+  {
+    $this->type = BannerType::validate($type, 'type');
   }
 
   public function render()
   {
     $render = [];
+
+    self::renderLink($render);
+
+    if (isset($this->message)) {
+      $render['message'] = $this->message;
+    }
+    if (isset($this->type)) {
+      $render['type'] = Functions::safeRender($this->type);
+    }
+
     return $render;
   }
 }
